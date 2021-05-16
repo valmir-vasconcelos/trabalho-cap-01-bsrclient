2c32dfd0a40f0040d94d1ffa4391b11ab7de3217
Valmir Vasconcelos <valmir.bsi@gmail.com>
2021-05-16 00:28:24 -0300

    project created

---
+++ backend/src/main/java/com/valmir/bsrclient/BsrclientApplication.java
@@ -0,0 +1,13 @@
+package com.valmir.bsrclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsrclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsrclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/valmir/bsrclient/BsrclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valmir.bsrclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsrclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
